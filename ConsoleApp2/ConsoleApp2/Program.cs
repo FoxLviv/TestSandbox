92d6213ae95bfbf7d256476d7749f801ad9d241d
INFOPULSE\Andrii.Burtso <Andrii.Burtso@infopulse.com>
2021-04-19 09:42:45 +0300

    Add example of IEnumerable vs IQuarible

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -13,8 +13,8 @@ namespace ConsoleApp2
             //typesExercises.Test2CloneReferenceType();
             //typesExercises.BoxingExample();
 
-            var listExercises = new ListExercises();
-            listExercises.Exercise();
+            //var listExercises = new ListExercises();
+            //listExercises.Exercise();
 
             //var dictionaryExample = new DictionaryExample();
             //dictionaryExample.Example();
@@ -25,6 +25,9 @@ namespace ConsoleApp2
             //var delAndEventExample = new DelegAndEventsExamples();
             //delAndEventExample.Example();
 
+            var enumerable = new EnumerationExample();
+            //enumerable.AllEx2();
+            enumerable.Example();
 
             Console.ReadKey();
         }
