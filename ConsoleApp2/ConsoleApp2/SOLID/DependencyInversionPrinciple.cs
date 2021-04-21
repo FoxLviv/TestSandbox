6d2528b95936e2237dc7e2a980f71387c9c3dbe1
INFOPULSE\Andrii.Burtso <Andrii.Burtso@infopulse.com>
2021-04-21 09:49:19 +0300

    Add Dependency Inversion Principle example

---
+++ ConsoleApp2/ConsoleApp2/SOLID/DependencyInversionPrinciple.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LearnBasics.Sandbox.SOLID
+{
+    class DependencyInversionPrinciple
+    {
+        public void Example()
+        {
+            var book = new Book(new ConsolePrinter());
+            book.Print();
+            book.Printer = new HtmlPrinter();
+            book.Print();
+        }
+    }
+
+    interface IPrinter
+    {
+        void Print(string text);
+    }
+
+    class Book
+    {
+        public string Text { get; set; }
+        public IPrinter Printer { get; set; }
+
+        public Book(IPrinter printer)
+        {
+            this.Printer = printer;
+        }
+
+        public void Print()
+        {
+            Printer.Print(Text);
+        }
+    }
+
+    class ConsolePrinter : IPrinter
+    {
+        public void Print(string text)
+        {
+            Console.WriteLine("Output in Console");
+        }
+    }
+
+    class HtmlPrinter : IPrinter
+    {
+        public void Print(string text)
+        {
+            Console.WriteLine("Output in HTML");
+        }
+    }
+}
