448005e07b51bba5ff2e2a6498c7dee5acd204aa
INFOPULSE\Andrii.Burtso <Andrii.Burtso@infopulse.com>
2021-04-21 09:29:52 +0300

    Add single responsibility principle example

---
+++ ConsoleApp2/ConsoleApp2/SOLID/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LearnBasics.Sandbox.SOLID
+{
+    class SingleResponsibilityPrinciple
+    {
+        public void Example()
+        {
+            var store = new MobileStore(
+                new ConsolePhoneReader(), new GeneralPhoneBinder(),
+                new GeneralPhoneValidator(), new TextPhoneSaver());
+            store.Process();
+        }
+    }
+    class Phone
+    {
+        public string Model { get; set; }
+        public int Price { get; set; }
+    }
+
+    class MobileStore
+    {
+        List<Phone> phones = new List<Phone>();
+
+        public IPhoneReader Reader { get; set; }
+        public IPhoneBinder Binder { get; set; }
+        public IPhoneValidator Validator { get; set; }
+        public IPhoneSaver Saver { get; set; }
+
+        public MobileStore(IPhoneReader reader, IPhoneBinder binder, IPhoneValidator validator, IPhoneSaver saver)
+        {
+            this.Reader = reader;
+            this.Binder = binder;
+            this.Validator = validator;
+            this.Saver = saver;
+        }
+
+        public void Process()
+        {
+            string[] data = Reader.GetInputData();
+            var phone = Binder.CreatePhone(data);
+            if (Validator.IsValid(phone))
+            {
+                phones.Add(phone);
+                Saver.Save(phone, "store.txt");
+                Console.WriteLine("Данные успешно обработаны");
+            }
+            else
+            {
+                Console.WriteLine("Некорректные данные");
+            }
+        }
+    }
+
+    interface IPhoneReader
+    {
+        string[] GetInputData();
+    }
+    class ConsolePhoneReader : IPhoneReader
+    {
+        public string[] GetInputData()
+        {
+            Console.WriteLine("Введите модель:");
+            string model = Console.ReadLine();
+            Console.WriteLine("Введите цену:");
+            string price = Console.ReadLine();
+            return new string[] { model, price };
+        }
+    }
+
+    interface IPhoneBinder
+    {
+        Phone CreatePhone(string[] data);
+    }
+
+    class GeneralPhoneBinder : IPhoneBinder
+    {
+        public Phone CreatePhone(string[] data)
+        {
+            if (data.Length >= 2)
+            {
+                int price = 0;
+                if (Int32.TryParse(data[1], out price))
+                {
+                    return new Phone { Model = data[0], Price = price };
+                }
+                else
+                {
+                    throw new Exception("Ошибка привязчика модели Phone. Некорректные данные для свойства Price");
+                }
+            }
+            else
+            {
+                throw new Exception("Ошибка привязчика модели Phone. Недостаточно данных для создания модели");
+            }
+        }
+    }
+
+    interface IPhoneValidator
+    {
+        bool IsValid(Phone phone);
+    }
+
+    class GeneralPhoneValidator : IPhoneValidator
+    {
+        public bool IsValid(Phone phone)
+        {
+            if (String.IsNullOrEmpty(phone.Model) || phone.Price <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+    }
+
+    interface IPhoneSaver
+    {
+        void Save(Phone phone, string fileName);
+    }
+
+    class TextPhoneSaver : IPhoneSaver
+    {
+        public void Save(Phone phone, string fileName)
+        {
+            using (var writer = new StreamWriter(fileName, true))
+            {
+                writer.WriteLine(phone.Model);
+                writer.WriteLine(phone.Price);
+            }
+        }
+    }
+}
