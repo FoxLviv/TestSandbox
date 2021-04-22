41951fb8fb4955e2b3cbfcd265de60477f845fa7
INFOPULSE\Andrii.Burtso <Andrii.Burtso@infopulse.com>
2021-04-22 23:26:43 +0300

    Add implementation for Dependency Injection

---
+++ ConsoleApp2/ConsoleApp2/Dependency injection/DL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnBasics.Sandbox.Dependency_injection
+{
+    class DL : IProduct
+    {
+        public string InsertData()
+        {
+            string val = "Dependency injection injected";
+            Console.WriteLine(val);
+            return val;
+        }
+    }
+}
--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
-﻿using LearnBasics.SandBox.BasicTypes;
+﻿using LearnBasics.Sandbox.Dependency_injection;
+using LearnBasics.SandBox.BasicTypes;
 using LearnBasics.SandBox.Collections;
 using LearnBasics.SandBox.DelegatesAndEvents;
 using LearnBasics.SandBox.Threads;
@@ -11,39 +12,41 @@ namespace LearnBasics.SandBox
     {
         static void Main(string[] args)
         {
-            var typesExercises = new TypesExercises();
-            int tmp = typesExercises.TestReferenceType();
+            //var typesExercises = new TypesExercises();
+            //int tmp = typesExercises.TestReferenceType();
 
+            //typesExercises.Test2CloneReferenceType();
+            //typesExercises.BoxingExample();
 
-            typesExercises.Test2CloneReferenceType();
-            typesExercises.BoxingExample();
+            //var listExercises = new ListExercises();
+            //listExercises.Exercise();
 
-            var listExercises = new ListExercises();
-            listExercises.Exercise();
+            //var dictionaryExample = new DictionaryExample();
+            //dictionaryExample.Example();
 
-            var dictionaryExample = new DictionaryExample();
-            dictionaryExample.Example();
+            //var stringExercise = new StringsExample();
+            //stringExercise.Example();
 
-            var stringExercise = new StringsExample();
-            stringExercise.Example();
+            //var delAndEventExample = new DelegAndEventsExamples();
+            //delAndEventExample.Example();
 
-            var delAndEventExample = new DelegAndEventsExamples();
-            delAndEventExample.Example();
+            //var threads = new ThreadsExample();
+            //threads.Example();
 
-            var threads = new ThreadsExample();
-            threads.Example();
+            //var linqExample = new LinqExample();
+            //linqExample.Example();
 
-            var linqExample = new LinqExample();
-            linqExample.Example();
+            //var xmlExam = new XMLExample();
+            //xmlExam.Example();
 
-            var xmlExam = new XMLExample();
-            xmlExam.Example();
+            //var jsonExample = new JSONExample();
+            //jsonExample.Example();
 
-            var jsonExample = new JSONExample();
-            jsonExample.Example();
+            //var file = new WorkWithFilesExample();
+            //file.Example();
 
-            var file = new WorkWithFilesExample();
-            file.Example();
+            var DIExample = new DIConfiguration();
+            DIExample.Main();
 
             Console.ReadKey();
         }
